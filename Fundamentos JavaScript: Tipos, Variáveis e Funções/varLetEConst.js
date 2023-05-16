5b9ac7dc4cc2c18f33e870aacacb0697ce2ac979
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-16 08:59:07 -0300

    Aula - Var, let e const

---
+++ Fundamentos JavaScript: Tipos, Variáveis e Funções/varLetEConst.js
@@ -0,0 +1,38 @@
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//var area = altura * comprimento;
+//console.log(area);
+//var area;
+
+//let
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if (forma === 'retangulo'){
+    area =  altura * comprimento;
+}else{
+    area = altura * comprimento/2;
+}
+
+console.log(area);
+
+//const
+
+const forma1 = 'retangulos';
+const altura1 = 5;
+const comprimento1 = 7;
+let area1 = altura1 * comprimento1;
+
+if (forma1 === 'retangulo'){
+    area1 = altura1 * comprimento1;
+}else{
+    area1 = altura1 * comprimento1/2
+}
+
+console.log(area1);
