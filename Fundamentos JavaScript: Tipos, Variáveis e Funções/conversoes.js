d0e639837522953ced979673a8f94a2c956d771e
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-17 14:49:39 -0300

    Aula conversões.

---
+++ Fundamentos JavaScript: Tipos, Variáveis e Funções/conversoes.js
@@ -0,0 +1,13 @@
+//Tipo de dado
+//Booleanos
+
+//conversão implicita
+const numero = 10;
+const numeroString = '10'
+
+//Number()
+//string()
+console.log(numero + numeroString);
+
+//conversão explicita
+console.log(numero + Number(numeroString));
