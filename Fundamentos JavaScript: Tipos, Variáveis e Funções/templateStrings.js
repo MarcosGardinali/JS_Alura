bd4266e298c83a5923b5b7de2aebb53466159c2b
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-23 09:44:27 -0300

    Ternários e Template Strings.

---
+++ Fundamentos JavaScript: Tipos, Variáveis e Funções/templateStrings.js
@@ -0,0 +1,7 @@
+//Template Strings
+
+const nome =  "Marcos";
+const idade = 21;
+const sou =  "Gamer";
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e eu sou ${sou}`;
+console.log(apresentacao);
---
+++ Fundamentos JavaScript: Tipos, Variáveis e Funções/ternario.js
@@ -0,0 +1,12 @@
+//Operadores ternários
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+/*if (idadeCliente >= idadeMinima){
+    console.log("Pode dirigir");
+}else{
+    console.log("Não pode dirigir");
+}*/
+            //Condição                      //True          //False
+console.log(idadeCliente >= idadeMinima ? "Pode dirigir" : "Não pode dirigir");
