1e5109392fdfa577aa15963e671606559cad9985
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-23 08:42:29 -0300

    Aula comparações.

---
+++ Fundamentos JavaScript: Tipos, Variáveis e Funções/comparacoes.js
@@ -0,0 +1,10 @@
+// == (Comparação implícita) Compara apenas os valores das variáveis;
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// == (Comparação explícita) Compara os valores e os tipos de dados das variáveis ;
+
+console.log(numero === texto);
